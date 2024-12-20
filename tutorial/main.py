b9fb4792d3c097f3b5c5d50d52898167c02b0f7e
Nelson Malbone <nhmalbone@icloud.com>
2024-12-20 17:05:29 -0500

    created a webapp to convert excel files to json files

--- tutorial/main.py
+++ tutorial/main.py
@@ -8,5 +8,20 @@ from excel_to_json import convert_excel_to_json
 
 
 st.title('Excel to Json File Convorter')
-st.write("Upload a excel file to convert")
+st.write("Upload an Excel file to Convert to a JSON format")
 
+
+uploaded_file = st.file_uploader('Choose an Excel file', type=['xlsx', 'xls'])
+# type allows to place the format of file to place insto drop box
+# xls older excel file format
+# xlsx newer excel file format
+
+# logical for if file is None  will not run
+if uploaded_file is not None:
+    json_data = convert_excel_to_json(uploaded_file)
+    st.json(json_data) # will display in json format on webpage
+
+    st.download_button(label="Download Json",
+                       data=json_data,
+                       file_name='converted.json',
+                       mime='application/json')
