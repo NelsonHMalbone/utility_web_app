92ac2e21327736a0e675075723a1096262b7eaaf
Nelson Malbone <nhmalbone@icloud.com>
2024-12-20 18:43:33 -0500

    created upload section created the download btn to download file.

--- student_project/main.py
+++ student_project/main.py
@@ -3,3 +3,13 @@ from excel_to_csv import excel_to_csv_convertor
 
 st.title("Excel to CSV File Converter")
 st.write('Upload an Excel file to Convert to a CSV format')
+
+# file selector
+upload_file = st.file_uploader('Choose an Excel File', type=['xlsx', 'xls'])
+
+if upload_file is not None:
+    csv_data = excel_to_csv_convertor(upload_file)
+    st.download_button(label='Download File',
+                       data=csv_data,
+                       file_name='converted.csv',
+                       mime='application/csv')
