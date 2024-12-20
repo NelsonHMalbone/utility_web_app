68e35081b78710991467ccb44963700dfdda50b0
Nelson Malbone <nhmalbone@icloud.com>
2024-12-20 16:48:30 -0500

    got the webpage running have a separate file for the script

---
+++ tutorial/excel_to_json.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import sys
+
+
+# requires a input of excel file
+def convert_excel_to_json(excel_file):
+
+    df = pd.read_excel(excel_file)
+    json_data = df.to_json(orient='records')
+
+    # output json data
+    return json_data
+
+# get rid of europe.json to print in console
+# print(convert_excel_to_json('europe.xlsx'))
+if __name__ == '__main__':
+    sys.exit()
--- tutorial/main.py
+++ tutorial/main.py
@@ -1,4 +1,12 @@
 import streamlit as st
-import pandas as pd
 import openpyxl as op
+from excel_to_json import convert_excel_to_json
+
+# difference between webapp and script
+# webapp requires a frontend and backend frame works excel_to_json.py is the backend main.py
+# is the front end best to keep these in separate files
+
+
+st.title('Excel to Json File Convorter')
+st.write("Upload a excel file to convert")
 
